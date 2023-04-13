55bf84ebf9f9b73f6dbdc7b599c65bbd7fb79143
ashish <apatel2@ualberta.ca>
2023-04-13 21:36:38 +0000

    recommit user-specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_13_924892) do
+ActiveRecord::Schema.define(version: 2023_04_13_924893) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2023_04_13_924892) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "widgets", force: :cascade do |t|
