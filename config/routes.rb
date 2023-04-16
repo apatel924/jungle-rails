673782a8026a3e5f8e431ebf888e6faed5411fae
ashish <apatel2@ualberta.ca>
2023-04-16 20:53:11 +0000

    small bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+
+ 
+
 Rails.application.routes.draw do
 
   root to: 'products#index'
@@ -5,11 +8,13 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  get '/signup' => 'sessions#new'
-  post '/users' => 'sessions#create'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:index, :new, :create]
+  resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -21,6 +26,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
